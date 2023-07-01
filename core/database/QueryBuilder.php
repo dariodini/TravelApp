218b884f197ba702f9134026c7847527c35ca9a0
Dario Di Nicuolo <dado02rio02@gmail.com>
2023-07-01 14:40:31 +0200

    Creati file per connessione al DB e query basilari. Connessione al db e file di conf salvati dentro App

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -1,1 +1,41 @@
+<?php
 
+namespace App\Core\Database;
+
+use PDO;
+
+class QueryBuilder
+{
+    protected $pdo;
+
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function selectAll($table)
+    {
+        $statement = $this->pdo->prepare("select * from {$table}");
+
+        $statement->execute();
+
+        return $statement->fetchAll(PDO::FETCH_CLASS);
+    }
+
+    public function insert($table, $parameters): void
+    {
+        $sql = sprintf(
+            'insert into %s (%s) values (%s)',
+            $table,
+            implode(', ', array_keys($parameters)),
+            ':' . implode(', :', array_keys($parameters))
+        );
+
+        try {
+            $statement = $this->pdo->prepare($sql);
+            $statement->execute($parameters);
+        } catch (\Exception $e) {
+            echo "An error occurred while executing the query. Try later.";
+        }
+    }
+}
