5f277e6e1a6232f7a444611a7779fd6c3915f6d9
Dario Di Nicuolo <dado02rio02@gmail.com>
2023-07-22 14:06:51 +0200

    Fix home

--- app/views/index.view.php
+++ app/views/index.view.php
@@ -1,5 +1,3 @@
 <?php require('partials/head.php'); ?>
 
-<div class="w-100 text-center alert alert-secondary mb-0 fs-4">Scegli dove andare!</div>
-
 <?php require('partials/footer-scripts.php'); ?>
